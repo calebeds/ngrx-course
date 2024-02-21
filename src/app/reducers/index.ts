b0c2d9ddd5579785c90c140a3a9c0fa7ccbb429f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-02-21 15:55:58 -0300

    add login action

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,12 @@
+import { isDevMode } from "@angular/core";
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer,
+} from "@ngrx/store";
+
+export interface AppState {}
+
+export const reducers: ActionReducerMap<AppState> = {};
