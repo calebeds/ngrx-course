ce0c0ba3459c0fc6e479d3a12c479e835bcc6e4c
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-02-22 12:15:42 -0300

    implement logout functionality

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -28,5 +28,10 @@ export const authReducer = createReducer(
     return {
       user: action.user,
     };
+  }),
+  on(AuthActions.logout, (state, action) => {
+    return {
+      user: undefined,
+    };
   })
 );
