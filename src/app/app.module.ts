9150c8590f676c79b45f0c4701d7faae4982107c
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-02-23 16:13:33 -0300

    setup ngrx route store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,6 +54,10 @@ const routes: Routes = [
     StoreModule.forRoot(reducers, {}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: "router",
+      routerState: RouterState.Minimal,
+    }),
   ],
   bootstrap: [AppComponent],
 })
